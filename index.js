783d9e58c06d0881aba6becfb32c17ce51a9e8b8
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2024-11-18 08:45:46 +0800

    solution-3

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+
 const todos = [
   { id: 1, title: "Buy milk", completed: false },
   { id: 2, title: "Buy bread", completed: true },
@@ -13,10 +15,21 @@ app.get("/", (req, res) => {
   res.send(`
 <main>
   <h1>My ugly todos !</h1>
+  <form action="/create-todo" method="post">
+    <input type="text" name="title" />
+    <button type="submit">Create</button>
+  </form>
   <ul>
     ${todos.map((todo) => `<li>${todo.title}</li>`).join("")}
   </ul>
 </main>`);
+});
+
+app.post("/create-todo", (req, res) => {
+  // get form data from request
+  const { title } = req.body;
+  todos.push({ id: todos.length + 1, title, completed: false });
+  res.redirect("/");
 });
 
 app.listen(port, () => {
